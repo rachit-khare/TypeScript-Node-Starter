d72936f6bff43d4ae9cb3979079f27c678cd01fd
Rachit Khare <rachit@freshworks.io>
2018-01-22 11:58:02 -0800

    Indent using 2 spaces - **** JS

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,12 +2,12 @@ import * as Sequelize from "sequelize";
 import * as sequelize from "../services/PostgresService";
 
 const User = sequelize.define('user', {
-    firstName: {
-        type: Sequelize.STRING
-    },
-    lastName: {
-        type: Sequelize.STRING
-    }
+	firstName: {
+		type: Sequelize.STRING
+	},
+	lastName: {
+		type: Sequelize.STRING
+	}
 });
 
 export = User;
